65906b40c91012b02813e263fad71671a2e9ca8a
Pablolinari <paablolinaari@gmail.com>
2024-10-01 13:08:24 +0200

    new project

---
+++ Multi-Threaded Stock Trading System in C++/Orderbook.h
@@ -0,0 +1,25 @@
+#ifndef ORDERBOOK_H
+#define ORDERBOOK_H
+
+#include <mutex>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Orderbook{
+private:
+	unordered_map<int, Stock > stocks;
+	unordered_map<int,Trader>traders;
+	unordered_map<int,vector<Orders> >orders;
+	mutable mutex mx;
+public:
+	Orderbook();
+	void addOrder(ArchivedOrder*);
+    void addStock(Stock*);
+    void removeOrder(ArchivedOrder*);
+    Stock* getStock(int) const;
+    Trader* getTrader(int) const;
+};
+
+#endif // !ORDERBOOK_H
